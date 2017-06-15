f9317ceed5772fafc39ff85812b94565d39e91f8
infosecstark <campagnanomattia@gmail.com>
2017-06-15 19:41:24 -0400

    Create antispam.py

--- antispam.py
+++ antispam.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python
 import os
+#import os module in order to use operating system functionalities
+
+#Have your system tell the bad guys where they have to go.
 os.system('say "Go to hell motherfucker" ')
